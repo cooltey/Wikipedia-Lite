1515bb07c96477c6c29531fabcbf76bb9e0ab155
cooltey <coolteygame@gmail.com>
2022-07-11 15:58:41 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/org/cooltey/wikipedia_lite/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.cooltey.wikipedia_lite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wikipedia Lite',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Wikipedia Lite'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: const <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+              child: Text(
+                "Welcome to Wikipedia Lite",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Search Wikipedia',
+                ),
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
